17ea0517c288a0a561a4750b87e5d7978eb3094f
Efren Tavarez <abetavarez@icloud.com>
2019-05-19 22:42:08 -0400

    Persisting data using Core Data. / New Table view for categories.

--- inQProject/TodoListViewController.swift
+++ inQProject/TodoListViewController.swift
@@ -13,6 +13,12 @@ class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
+    
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -24,10 +30,6 @@ class TodoListViewController: UITableViewController {
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         
-        loadItems()
-        
-      
-  
     }
 
     //MARK - Tableview Datasource Methods
@@ -50,6 +52,12 @@ class TodoListViewController: UITableViewController {
         return cell
     }
 
+    
+    
+    
+    
+    
+    
     //MARK - Tableview  Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -63,6 +71,14 @@ class TodoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    
+    
+    
+    
+    
+    
+    
     
     //MARK - Add New Items
     
@@ -80,6 +96,7 @@ class TodoListViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             
@@ -101,12 +118,17 @@ class TodoListViewController: UITableViewController {
         
     }
     
+    
+    
+    
+    
+    
+    
+    
     //MARK - Model Manipulation Methods
     
     func saveItems() {
-        
-       
-        
+    
         do {
 
             try context.save()
@@ -119,8 +141,15 @@ class TodoListViewController: UITableViewController {
     
     
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
-
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(),predicate: NSPredicate? = nil) {
+        
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
         
         do {
         itemArray = try context.fetch(request)
@@ -138,18 +167,28 @@ class TodoListViewController: UITableViewController {
 }
 
 
+
+
+
+
+
+
+
 //MARK: - Search bat Methods
 
 extension TodoListViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
         let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        
        
-        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
 
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
         
-        loadItems(with: request)
+        loadItems(with: request,predicate: predicate)
     
     }
     
